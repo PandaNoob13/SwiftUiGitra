257cc9f86403ad3089f670efe5fb901ddc98dd58
PandaNoob13 <61651534+PandaNoob13@users.noreply.github.com>
2021-09-10 02:01:16 +0700

    Initial Commit

---
+++ SwiftUiGitra/SwiftUiGitraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiGitraApp.swift
+//  SwiftUiGitra
+//
+//  Created by Samuel Maynard on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiGitraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
